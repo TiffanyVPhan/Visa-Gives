55ed91a7f7910bbf65e81da96367e09024d5e22a
TiffanyVPhan <tiffany.phan@colorado.edu>
2020-06-23 13:50:06 -0600

    added some input validation

--- covid-charities/src/app/components/create-account/create-account.component.ts
+++ covid-charities/src/app/components/create-account/create-account.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, NgForm } from '@angular/forms';
 import { Account } from '../../model/account';
 import { AccountService } from '../../services/account.service';
 
@@ -16,32 +16,26 @@ export class CreateAccountComponent implements OnInit {
   email: string;
   image: string;
   submit = false;
+  accountForm: FormGroup;
 
-  accountForm = new FormGroup({
-     first: new FormControl(this.firstName, Validators.required),
-     last: new FormControl(this.lastName, [Validators.required]),
-     pass: new FormControl(this.password, [Validators.required]),
-     confirm: new FormControl(this.confirmPassword, [Validators.required]),
-     email_: new FormControl(this.email, [Validators.required]),
-     profilePicture: new FormControl(this.image)
-   });
   constructor(private accountService: AccountService,
               private fb: FormBuilder) {}
 
   ngOnInit() {
-    // this.accountForm = this.fb.group({
-    //   firstName: ['', Validators.required],
-    //   lastName: ['', Validators.required],
-    //   password: ['', Validators.required],
-    //   confirmPassword: ['', Validators.required],
-    //   email: ['', Validators.required]
-    // });
+    this.accountForm = new FormGroup({
+      first: new FormControl(this.firstName, Validators.required),
+      last: new FormControl(this.lastName, [Validators.required]),
+      pass: new FormControl(this.password, [Validators.required]),
+      confirm: new FormControl(this.confirmPassword, [Validators.required]),
+      email_: new FormControl(this.email, [Validators.required]),
+      profilePicture: new FormControl(this.image)
+    });
     console.log(this.accountForm);
   }
 
-  onSubmit(): void {
+  onSubmit(form: NgForm): void {
     // TO-DO: this probably pings backend to create a new account
-    console.log(this.accountForm.value);
+    console.log(form);
     this.submit = true;
   }
 }
