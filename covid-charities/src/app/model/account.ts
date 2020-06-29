794bd7b7cefe7fb2bf67ab115ec7d340e639f82a
TiffanyVPhan <tiffany.phan@colorado.edu>
2020-06-29 03:44:27 -0600

    fixed model

--- covid-charities/src/app/model/account.ts
+++ covid-charities/src/app/model/account.ts
@@ -1,13 +1,13 @@
-export type donationHistoryItem = {
-    amount: number,
-    charityID: string,
-    dateDonated
-}
+// export type donationHistoryItem = {
+//     amount: number,
+//     charityID: string,
+//     dateDonated
+// }
 export class Account {
     constructor(public firstName: string,
                 public lastName: string,
                 public interests: string [],
-                public donationHistory: donationHistoryItem [],
+                public donationHistory: string [],
                 public profilePicture: string,
                 public totalAmountDonated: number,
                 public email: string,
